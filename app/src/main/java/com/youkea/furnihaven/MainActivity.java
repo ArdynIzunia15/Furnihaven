2da333038256bf9a7ac12622ca55b71664cb15b0
ArdynIzunia15 <navs150103@gmail.com>
2023-09-29 21:30:34 +0700

    first commit

---
+++ app/src/main/java/com/youkea/furnihaven/MainActivity.java
@@ -0,0 +1,50 @@
+package com.youkea.furnihaven;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class MainActivity extends AppCompatActivity {
+    MaterialButton btnView3D;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnView3D = findViewById(R.id.btnView3D);
+
+        // Firebase Storage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference();
+
+        btnView3D.setOnClickListener(v -> {
+            // Get Download Url
+            storageReference.child("Kursi 1.gltf").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    // Open ARCore Intent
+                    Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+                    Uri intentUri =
+                            Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
+                                    .appendQueryParameter("file", uri.toString())
+                                    .appendQueryParameter("mode", "3d_preferred")
+                                    .build();
+                    sceneViewerIntent.setData(intentUri);
+                    sceneViewerIntent.setPackage("com.google.ar.core");
+                    startActivity(sceneViewerIntent);
+                }
+            });
+        });
+    }
+}
